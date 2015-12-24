e4ebe78a636ee11db3f18e9e5760607874cbb575
wq <539921311@qq.com>
2015-12-24 17:28:31 +0800

    wechat from desktop

---
+++ template/js/render.js
@@ -0,0 +1,9 @@
+//海淘数据render
+function renderHaitao(data, node){
+  var mydata = '<div style="height:200px;margin:20px;background-color:black;"></div>';//拼接HTML,@万能的超
+  node.append(mydata);
+  node.append(mydata);
+  node.append(mydata);
+  node.append(mydata);
+  node.append(mydata);
+}
