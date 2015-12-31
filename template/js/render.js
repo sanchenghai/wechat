f661a49a7f1f8140d7ea361355c131e2381684ef
wq <539921311@qq.com>
2015-12-31 12:01:13 +0800

    change haitao

--- template/js/render.js
+++ template/js/render.js
@@ -10,7 +10,7 @@ function renderHaitao(data, node){
 
 
 
-//促销热门数据render
+//促销商城数据render
 function renderCuxiaoshangcheng(data, node){
   var mydata = '<div style="height:200px;margin:20px;background-color:black;"></div>';//拼接HTML,@万能的超
   node.append(mydata);
